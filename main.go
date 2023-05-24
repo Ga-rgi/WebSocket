0237aa3d78ed680cd6faef1c046e5171afd8ec4e
Gargi <gargi@roverx.team>
2023-05-24 15:19:41 +0530

    websocket

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"log"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ws", WebSocketConnect)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Printf("failed to start server: %v", err)
+		panic(err)
+	}
+}
+
+func WebSocketConnect(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("Failed to upgrade connection to WebSocket:", err)
+		return
+	}
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message:", err)
+			break
+		}
+		log.Printf("Received message: %s\n", msg)
+
+		err = conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			log.Println("Error writing message:", err)
+			break
+		}
+		log.Printf("Sent Message %s\n", msg)
+	}
+}
